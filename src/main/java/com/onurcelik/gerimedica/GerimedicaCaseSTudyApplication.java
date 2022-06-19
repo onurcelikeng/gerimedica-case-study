da0e5aa462ab1b49cd6a6966198df7844c152abd
Onur Celik <onrcelik@outlook.com>
2022-06-19 13:56:06 +0300

    init commit

---
+++ src/main/java/com/onurcelik/gerimedica/GerimedicaCaseSTudyApplication.java
@@ -0,0 +1,13 @@
+package com.onurcelik.gerimedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerimedicaCaseSTudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerimedicaCaseSTudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurcelik/gerimedica/GerimedicaCaseSTudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurcelik.gerimedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerimedicaCaseSTudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
